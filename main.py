fd11c6086fd5666025bc39db183fd86ca3ca7620
Kevin Williams <williakw@Hotmail.com>
2023-05-16 12:06:12 +0200

    test

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from tqdm.notebook import tqdm
+from keras.preprocessing.image import img_to_array, load_img
+import tensorflow as tf
+import matplotlib.pyplot as plt
+#%matplotlib
+#use okt.show
+import warnings
+
+warnings.filterwarning('ignore')
